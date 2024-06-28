1df783a1afd3e682e44a6892d1f7d7395a4322c5
mariDev <nahuemarcilli@gmail.com>
2024-06-28 16:15:08 -0300

    implement dollar appi GUI interface

--- dolarapi.py
+++ dolarapi.py
@@ -1,19 +1,18 @@
-from flask import Flask, render_template, request
+# dolarapi.py
+import requests
 
-
-url = 'https://api.bluelytics.com.ar/v2/latest'
-response = requests.get(url)
-#class dolar: falta definir la api como objeto
-if response.status_code == 200:
-    # Obtén los datos JSON de la respuesta
+def obtener_cotizacion_dolar():
+    url = 'https://api.bluelytics.com.ar/v2/latest'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
         dolar = response.json()
-        #data['oficial']
-        #data['blue']
-
-blue_string = []
-for elemento in dolar['blue']:
-    blue_string.append(str(dolar['blue'].get(elemento)))
-
-oficial_string = []
-for elemento in dolar['oficial']:
-    oficial_string.append(str(dolar['oficial'].get(elemento)))
+        blue = dolar['blue']
+        oficial = dolar['oficial']
+        
+        blue_string = [str(blue.get('value_avg')), str(blue.get('value_sell')), str(blue.get('value_buy'))]
+        oficial_string = [str(oficial.get('value_avg')), str(oficial.get('value_sell')), str(oficial.get('value_buy'))]
+        
+        return blue_string, oficial_string
+    else:
+        return [], []
--- main.py
+++ main.py
@@ -1,56 +1,81 @@
 from flask import Flask, render_template, request
+import requests
+
 from vehiculos import *
 from clientes import *
 from transacciones import *
-from cotizacion import *
-
+#from dolarapi import *
 
 app = Flask(__name__, template_folder="templates")
 
+# Variables globales
 
-global opcion_menu_ppal , opcion_menu_vechiculos ,opcion_menu_clientes, opcion_menu_ppal_transaccion 
-global sub_op_menu_vehiculo, sub_op_menu_cliente, sub_op_menu_transacciones
-
+# Función para obtener la cotización del dólar
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():# menu ppal
+def index():
+    global opcion_menu_ppal
+    
     lista_menu = [
-    '1. Vehículos',
-    '2. Clientes', 
-    '3. Transacciones', 
-    '4. Ver Cotización del Dolar',
-    'Menú Principal',
-    'Ir al Menú',#h3
-    'Concesionario La Ñata',#h1
-    'Bienvenido!'] #h2
+        '1. Vehículos',
+        '2. Clientes', 
+        '3. Transacciones', 
+        '4. Ver Cotización del Dolar',
+        'Menú Principal',
+        'Ir al Menú',
+        'Concesionario La Ñata',
+        'Bienvenido!'
+    ]
      
     opcion_menu_ppal = request.form.get('opcion')
     
-    return render_template('index.html', **{'lista_menu': lista_menu})
+    return render_template('index.html', lista_menu=lista_menu)
+
 @app.route('/vehiculos', methods=['GET', 'POST'])
-def vehiculos_ppal():# menu vehiculos
+def vehiculos_ppal():
     return vehiculos()
+
 @app.route('/clientes', methods=['GET', 'POST'])
-def clientes_ppal():# menu clientes
-   return clientes()
+def clientes_ppal():
+    return clientes()
+
 @app.route('/transacciones', methods=['GET', 'POST'])
-def transacciones_ppal():# menu transacciones
+def transacciones_ppal():
     return transacciones()
+
 @app.route('/cotizacion', methods=['GET', 'POST'])
-def cotizacion_ppal():# menu cotizacion
-    return cotizacion()
-#Terminan Menus Principales#
+def cotizacion_ppal():
+    blue_string, oficial_string = obtener_cotizacion_dolar()
 
+    lista_menu =  [
+    'Cotizacion del Dolar',
+        'Dolar Blue',
+        'Dolar Oficial',
+        'Volver al Menu Principal',
+        'Concesionario La Ñata',#h1
+        'Bienvenido!'] #h2 
 
+    headers = ['Dolar Promedio','Dolar Venta','Dolar Compra']
+    fields1 = [blue_string]  # Dólar Blue
+    fields2 = [oficial_string]  # Dólar Oficial
+        
+    return render_template('cotizacion.html', **{'lista_menu': lista_menu, 'headers': headers, 'fields1':fields1, 'fields2':fields2})
 
-
-
-
+        #Integrar la plantilla cotizacion.html proporcionada
+def obtener_cotizacion_dolar():
+    url = 'https://api.bluelytics.com.ar/v2/latest'
+    response = requests.get(url)
     
-
-
-
-
-if __name__ == '__main__':
-    app.run()
+    if response.status_code == 200:
+        dolar = response.json()
+        blue = dolar['blue']
+        oficial = dolar['oficial']
+        
+        blue_string = [str(blue.get('value_avg')), str(blue.get('value_sell')), str(blue.get('value_buy'))]
+        oficial_string = [str(oficial.get('value_avg')), str(oficial.get('value_sell')), str(oficial.get('value_buy'))]
+        
+        return blue_string, oficial_string
+    else:
+        return [], []
+    
