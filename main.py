4961d2bf6968cd3d0a0ee62d302eeb799e6dad4b
e-conte <160084751+e-conte@users.noreply.github.com>
2024-06-28 23:29:38 -0300

    All in main

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template, redirect, url_for
+from flask import request
 
-import requests
 import json
 
-from vehiculos import *
-from clientes import *
-from transacciones import *
-#from dolarapi import *
 
 # Abrir y cargar el archivo clientes.json
 with open('clientes.json', 'r') as clientes_file:
@@ -17,12 +13,11 @@ with open('vehiculos.json', 'r') as vehiculos_file:
     vehiculos = json.load(vehiculos_file)
 
 
-
 app = Flask(__name__, template_folder="templates",static_folder='../static')
 app.config['FLASK_SKIP_CSRF'] = True
 app.static_folder = 'static'
 
-global lista_menu
+#global lista_menu
 
 # Variables globales
 
@@ -46,76 +41,92 @@ def index():
 
 
 ###############################################################
-
 @app.route('/vehiculos', methods=['GET', 'POST'])
-def vehiculos_ppal():
-    # def vehiculos():
-     lista_menu = [
-     '1. Crear Vehículos', 
-     '2. Editar Vehículos', 
-     '3. Eliminar Vehículo', 
-     '4. Listar Vehículo', 
-     '5. Buscar Vehículo',
-     '6. Volver al Menú Principal',
-     'Vehículos',
-     'Ir al Menú',
-     'Concesionario La Ñata',#h1
-     'Bienvenido!'] #h2
- 
-     opcion_menu_vehiculos = request.form.get('opcion')
- 
-     return render_template('vehiculos.html', **{'lista_menu': lista_menu})
-   # return vehiculos()
+def vehiculos():
+    lista_vehiculos = [
+    '1. Crear Vehículos', 
+    '2. Editar Vehículos', 
+    '3. Eliminar Vehículo', 
+    '4. Listar Vehículo', 
+    '5. Buscar Vehículo',
+    '6. Volver al Menú Principal',
+    'Vehículos',
+    'Ir al Menú',
+    'Concesionario La Ñata',#h1
+    'Bienvenido!'] #h2
+
+    #opcion_menu_vehiculos = request.form.get('opcion')
+
+    return render_template('vehiculos.html', **{'lista_vehiculos':lista_vehiculos})
 
 @app.route('/vehiculos-crear', methods=['GET', 'POST'])
-#def vehiculos_crear():
-#    return vehiculosCrear()
+def vehiculos_crear():
+    lista_crear = [    
+    'Concesionario La Ñata',#h1
+    'Bienvenido!', #h2
+    'Vehículo Nuevo'] #h3
 
-#@app.route('/vehiculos-crear', methods=['POST'])
+    return render_template('vehiculos-crear.html',**{'lista_crear' : lista_crear} )
 
-#def update_json(file_path, new_dictionary):
-#    update_json('vehiculos.json', auto_nuevo)
-#    with open(file_path, 'r+') as f:
-#        data = json.load(f)
-#        data.update(new_dictionary)
-#        f.seek(0)
-#        json.dump(data, f, indent=4)
-
-
-def guardar_auto_nuevo():
-    formulario = request.form
-    auto_nuevo = dict(formulario)
-    
-#    update_json('vehiculos.json', auto_nuevo)
-    with open(vehiculos.json, 'r+') as f:
-        data = json.load(f)
-
-    with load(vehiculos.json, 'r+') as f:
-        data = json.load(f)
-        data.update(auto_nuevo)
-        f.seek(0)
-        json.dump(data, f, indent=4)
-
- #   update_json('vehiculos.json', auto_nuevo)
-
-    return 'Datos del formulario guardados en el diccionario.'
+def submit():
+    if request.method == 'POST':
+        form_data = request.form.to_dict()
+        #   guardo data
+        with open('vehiculos.json', 'a') as file:
+            json.dump(form_data, file)
+            file.write('\n')
+        #   guardo data en json
+        return redirect(url_for('vehiculos'))
 
 
 
 
-#################################################################
+
+
+
+
+
+
+
+
+
+
 
 @app.route('/clientes', methods=['GET', 'POST'])
-def clientes_ppal():
-    return clientes()
+def clientes():
+    lista_menu = [
+    '1. Crear Cliente', 
+    '2. Editar Cliente', 
+    '3. Eliminar Cliente', 
+    '4. Listar Clientes', 
+    '5. Buscar Clientes', 
+    '6. Volver al Menu Principal',
+    'Menu Clientes',
+    'Ir al Menú',
+    'Concesionario La Ñata',#h1
+    'Bienvenido!'] #h2
 
+
+    opcion_menu_clientes = request.form.get('opcion')
+
+    return render_template('clientes.html', **{'lista_menu': lista_menu})
 
 @app.route('/transacciones', methods=['GET', 'POST'])
-def transacciones_ppal():
-    return transacciones()
+def transacciones():
+    lista_menu = [
+    '1. Crear Transacción', 
+    '2. Listar Transacciones',
+    '3. Buscar Transacción', 
+    '4. Volver al Menú Principal',
+    'Menu Transacciones',
+    'Ir al Menú',
+    'Concesionario La Ñata',#h1
+    'Bienvenido!'] #h2
 
 
+    opcion_menu_transacciones = request.form.get('opcion')
 
+    return render_template('transacciones.html', **{'lista_menu': lista_menu})
 
 #################### DOLAR OKEY ####################
 @app.route('/cotizacion', methods=['GET', 'POST'])
